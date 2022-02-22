c34309008e2a4d9be727d4e784f712ab4d0e6ba4
Pedro Mello <contato@cicatriz.dev>
2022-02-22 11:04:19 -0300

    feat: add twrnc support

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
-    }
-    config.resolve.extensions = [
-      '.web.js',
-      '.web.jsx',
-      '.web.ts',
-      '.web.tsx',
-      ...config.resolve.extensions,
-    ]
-    return config
-  },
-}
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['twrnc']);
+
+module.exports = withPlugins([withTM], {
+	webpack: (config) => {
+		config.resolve.alias = {
+			...(config.resolve.alias || {}),
+			// Transform all direct `react-native` imports to `react-native-web`
+			'react-native$': 'react-native-web',
+		};
+		config.resolve.extensions = ['.web.js', '.web.ts', '.web.tsx', ...config.resolve.extensions];
+		return config;
+	},
+});
